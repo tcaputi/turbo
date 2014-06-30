919c172c46d2f2cc6c297109182a2538a3ecb90c
Sandile Keswa <sandile@172-26-106-0.dynapool.nyu.edu>
2014-06-29 23:53:24 -0400

    its allllll gud mah nigga

--- pathtree.go
+++ pathtree.go
@@ -15,7 +15,17 @@ type PathTreeNode struct {
 	depth    int
 }
 
-func (tree *PathTree) put(path string) {
+func (tree *PathTree) path(path string) string {
+	return "/" + strings.Trim(path, "/")
+}
+
+func (tree *PathTree) put(path string) *PathTreeNode {
+	path = tree.path(path)
+	// First check if this path represents a node already
+	if tree.refs[path] != nil {
+		return tree.refs[path]
+	}
+
 	node := PathTreeNode{
 		parent:   nil,
 		children: make(map[*PathTreeNode]bool),
@@ -23,26 +33,61 @@ func (tree *PathTree) put(path string) {
 		depth:    strings.Count(path, "/"),
 	}
 	tree.refs[path] = &node
-
 	// Check relationship with the heads
 	parent := tree.parent(path)
-	if parent != nil {
-		node.parent = parent
-		parent.children[&node] = true
+	if parent == nil {
+		parent = &PathTreeNode{
+			parent:   nil,
+			children: make(map[*PathTreeNode]bool),
+			path:     "/",
+			depth:    0,
+		}
+		tree.refs["/"] = parent
 	}
+
+	node.parent = parent
+	// Check if new parent has any of the new node's children
+	for child, _ := range parent.children {
+		if child != nil {
+			if child.depth >= (node.depth + 1) {
+				// Potential child of new node
+				if strings.Index(child.path, node.path) == 0 {
+					// Child should have this node as a parent
+					delete(parent.children, child)
+					child.parent = &node
+					node.children[child] = true
+				}
+			}
+		}
+	}
+	parent.children[&node] = true
+
+	return &node
 }
 
 func (tree *PathTree) parent(path string) *PathTreeNode {
-	depth := strings.Count(path, "/")
-
-	if depth <= 1 {
+	path = tree.path(path)
+	// First check if this path represents a node already
+	node := tree.refs[path]
+	if node != nil && node.parent != nil {
+		return node.parent
+	}
+	// Check if its the root path
+	if path == "/" {
 		return nil
 	}
-
+	// Establish depth to loop
+	depth := strings.Count(path, "/")
+	// Recursively check every parent path
 	for i := 0; i < depth; i++ {
 		index := strings.LastIndex(path, "/")
-		if index > 0 {
+		if index != -1 {
 			path = path[:index]
+			// Mitigate against surface level paths
+			if len(path) == 0 {
+				path = "/"
+			}
+			// Return the parent if it exists
 			if tree.refs[path] != nil {
 				return tree.refs[path]
 			}
@@ -66,17 +111,6 @@ func (tree *PathTree) children(path string) *map[*PathTreeNode]bool {
 	}
 }
 
-func (tree *PathTree) allChildren(path string) *map[string]bool {
-	node := tree.refs[path]
-	list := make(map[string]bool)
-	if node != nil {
-		tree.allChildrenHelper(&list, node)
-		return &list
-	} else {
-		return nil
-	}
-}
-
 func (tree *PathTree) kill(path string) *map[string]bool {
 	node := tree.refs[path]
 	list := make(map[string]bool)
@@ -86,12 +120,5 @@ func (tree *PathTree) kill(path string) *map[string]bool {
 		return &list
 	} else {
 		return nil
-	}
-}
-
-func (tree *PathTree) allChildrenHelper(list *map[string]bool, node *PathTreeNode) {
-	for child, _ := range node.children {
-		tree.allChildrenHelper(list, child)
-		(*list)[child.path] = true
 	}
 }
--- pathtree_test.go
+++ pathtree_test.go
@@ -4,35 +4,98 @@ import (
 	"testing"
 )
 
-var (
-	tree = &PathTree{
+func TestParent(t *testing.T) {
+	tree := &PathTree{
 		refs: make(map[string]*PathTreeNode),
 	}
-)
 
-func TestPut(t *testing.T) {
-	tree.put("a/b/c")
-	tree.put("a/b/c/d/e/f/g/h")
-	tree.put("a/b/c/d/e/f")
-	tree.put("a/b/c/d/e/f/g/h")
+	tree.put("/a/b/c")
+	tree.put("a/b/c/d/e/f/g/h/")
+	tree.put("/a/b/c/d/e/f")
+	tree.put("/a/b/c/d/e/f/g/h")
+	tree.put("a/b/c/d/e/f/1")
+	tree.put("a/b/c/d/e/f/1/2")
 	tree.put("a/b/c/d/e/f/g/h/i/j")
-	tree.put("a/b/c/d")
-}
+	tree.put("/a/b/c/d/")
 
-func TestParent(t *testing.T) {
-	if tree.parent("a/b/c/d").path != "a/b/c" {
-		t.Fail()
+	par1 := tree.parent("a/b/c/d")
+	par2 := tree.parent("a/b/c/d/e/f/g/h/")
+	par3 := tree.parent("/a/b/c/d/e/f/g/h/i/j")
+	par4 := tree.parent("a")
+
+	if par1 == nil {
+		t.Error("par1 was nil")
+		t.FailNow()
 	}
-	if tree.parent("a/b/c/d/e/f/g/h").path != "a/b/c/d/e/f" {
-		t.Fail()
+	if par2 == nil {
+		t.Error("par2 was nil")
+		t.FailNow()
 	}
-	if tree.parent("a/b/c/d/e/f/g/h/i/j").path != "a/b/c/d/e/f/g/h" {
-		t.Fail()
+	if par3 == nil {
+		t.Error("par3 was nil")
+		t.FailNow()
 	}
-	if tree.parent("a") != nil {
-		t.Fail()
+	if par4 == nil {
+		t.Error("par4 was nil")
+		t.FailNow()
+	}
+
+	if par1.path != "/a/b/c" {
+		t.Error("par1 path was wrong", par1.path)
+	}
+	if par2.path != "/a/b/c/d/e/f" {
+		t.Error("par2 path was wrong", par2.path)
+	}
+	if par3.path != "/a/b/c/d/e/f/g/h" {
+		t.Error("par3 path was wrong", par3.path)
+	}
+	if par4.path != "/" {
+		t.Error("par4 path was wrong", par4.path)
 	}
 }
 
 func TestChildren(t *testing.T) {
+	tree := &PathTree{
+		refs: make(map[string]*PathTreeNode),
+	}
+
+	tree.put("/a/b/c")
+	tree.put("a/b/c/d/e/f/g/h/")
+	tree.put("/a/b/c/d/e/f")
+	tree.put("/a/b/c/d/e/f/g/h")
+	tree.put("a/b/c/d/e/f/1")
+	tree.put("a/b/c/d/e/f/1/2")
+	tree.put("a/b/c/d/e/f/g/h/i/j")
+	tree.put("/a/b/c/d/")
+
+	chil1 := tree.children("/a/b/c/d")
+	chil2 := tree.children("/a/b/c/d/e/f")
+	chil3 := tree.children("/a/b/c/d/e/f/g/h")
+	chil4 := tree.children("/a/b")
+
+	if chil1 == nil {
+		t.Error("chil1 was nil", tree.refs)
+		t.FailNow()
+	}
+	if chil2 == nil {
+		t.Error("chil2 was nil")
+		t.FailNow()
+	}
+	if chil3 == nil {
+		t.Error("chil3 was nil")
+		t.FailNow()
+	}
+
+	if len(*chil1) != 1 {
+		t.Error("chil1", len(*chil1))
+	}
+	if len(*chil2) != 2 {
+		t.Error("chil2", len(*chil2))
+	}
+	if len(*chil3) != 1 {
+		t.Error("chil3", len(*chil3))
+	}
+	if chil4 != nil {
+		t.Error("chil4", chil4)
+	}
 }
